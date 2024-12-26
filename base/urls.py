633b58e600ba09a8fe448c11446e220af63b2cc7
whoami <geehyness22@gmail.com>
2024-12-26 12:54:59 +0200

    reset

--- base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MTList, Message
-
-urlpatterns = [
-    path('', MTList.as_view(), name='home'),
-    path('message/<int:pk>', Message.as_view(), name='message'),
-]
